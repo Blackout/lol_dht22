3d2a8a1345cd820f66e03f997778328110a0f6aa
root <root@flowerpi.home>
2019-12-06 09:32:59 +1000

    check temp and humidity are within range. -3276.7 temp was being return and 6554 humidity

--- dht22.c
+++ dht22.c
@@ -75,14 +75,18 @@ static int read_dht22_dat(int count)
 
   // check we read 40 bits (8bit x 5 ) + verify checksum in the last byte
   // print it out if data is good
-  if ((j >= 40) && 
-      (dht22_dat[4] == ((dht22_dat[0] + dht22_dat[1] + dht22_dat[2] + dht22_dat[3]) & 0xFF)) ) {
+  if (	(j >= 40) && 
+      	(dht22_dat[4] == ((dht22_dat[0] + dht22_dat[1] + dht22_dat[2] + dht22_dat[3]) & 0xFF))
+	
+  ) {
         float t, h;
         h = (float)dht22_dat[0] * 256 + (float)dht22_dat[1];
         h /= 10;
         t = (float)(dht22_dat[2] & 0x7F)* 256 + (float)dht22_dat[3];
         t /= 10.0;
         if ((dht22_dat[2] & 0x80) != 0)  t *= -1;
+
+	if (h > 100 || t < -100) return 0;
 
 
     printf("{\"humidity\": %f, \"temperature\": %f, \"attempts\": %d}\n", h, t, count );
